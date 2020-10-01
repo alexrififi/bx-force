dab199a67dd462b5a4a2a4ba7fc8a8e2feb54344
Aleksandr Medvedev <s@lifestar.ru>
2020-10-01 09:16:50 +0500

    Добавлен EventServiceProvider

---
+++ src/Providers/EventServiceProvider.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Medvinator\BxForce\Providers;
+
+use Bitrix\Main\EventManager;
+
+class EventServiceProvider
+{
+    protected const BX_MODULES = [
+        'iblock',
+        'main',
+        'search',
+    ];
+
+    /**
+     * @var EventManager
+     */
+    protected $eventManager;
+
+    public function __construct()
+    {
+        $this->eventManager = EventManager::getInstance();
+    }
+
+    public static function make(): void
+    {
+        (new static())->boot();
+    }
+
+    /**
+     * Регистрация обработчиков событий
+     */
+    protected function boot(): void
+    {
+        foreach (self::BX_MODULES as $module) {
+            if ( method_exists( $this, $module ) ) {
+                $this->$module();
+            }
+        }
+    }
+
+    /**
+     * @param string         $module
+     * @param string         $event
+     * @param array|callable $callback
+     */
+    protected function register(string $module, string $event, $callback): void
+    {
+        $this->eventManager->addEventHandler( $module, $event, $callback );
+    }
+}
