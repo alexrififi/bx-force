0188609e046a19c7d64a373cdf9cfbd7d40bce48
Aleksandr Medvedev <s@lifestar.ru>
2020-10-16 14:18:22 +0500

    Исправлена несовместимость с командой orm:annotate

--- src/Providers/EventServiceProvider.php
+++ src/Providers/EventServiceProvider.php
@@ -3,6 +3,7 @@
 namespace Medvinator\BxForce\Providers;
 
 use Bitrix\Main\EventManager;
+use function defined;
 
 class EventServiceProvider
 {
@@ -23,8 +24,24 @@ class EventServiceProvider
         $this->eventManager = EventManager::getInstance();
     }
 
-    public static function make(): void
+    /**
+     * @param string|null $module
+     */
+    public static function make($module = null): void
     {
+        if ( defined( 'DisableEventsCheck' ) && DisableEventsCheck ) {
+            return;
+        }
+
+        if ( $module ) {
+            $moduleServiceProviderClass = ucwords( str_replace( '.', ' ', $module ) );
+            $moduleServiceProviderClass = str_replace( ' ', '\\', $moduleServiceProviderClass ) . '\\Providers\\EventServiceProvider';
+            if ( class_exists( $moduleServiceProviderClass ) ) {
+                (new $moduleServiceProviderClass)->boot();
+                return;
+            }
+        }
+
         (new static())->boot();
     }
 
