fbf9e251fe949e865a96b4396475b468afeb5be1
Aleksandr Medvedev <s@lifestar.ru>
2020-11-26 17:27:39 +0500

    Config: добавлена возможность получать конфиг вне модуля

--- src/Support/Config.php
+++ src/Support/Config.php
@@ -6,9 +6,44 @@ use Bitrix\Main\Application;
 use Bitrix\Main\Config\Configuration;
 use Illuminate\Support\Str;
 
+/**
+ * @method static get(string $path)
+ */
 class Config
 {
-    protected static $modules = [];
+    protected $configurations;
+    protected $module;
+
+    /**
+     * Handle dynamic static method calls
+     *
+     * @param string $method
+     * @param array  $parameters
+     * @return mixed
+     */
+    public static function __callStatic(string $method, array $parameters)
+    {
+        if ( $method === 'get' ) {
+            return self::getInstance()->value( ...$parameters );
+        }
+        return self::getInstance()->$method( ...$parameters );
+    }
+
+    protected static function getInstance(): Config
+    {
+        static $instance = null;
+        if ( $instance === null ) {
+            $instance = new static();
+        }
+        return $instance;
+    }
+
+    public static function forModule(string $module): Config
+    {
+        $instance = new static;
+        $instance->module = $module;
+        return $instance;
+    }
 
     /**
      * Получить значение настройки
@@ -16,23 +51,23 @@ class Config
      * @param string $path Путь до конкретной настройки. Доступна "dot" нотация
      * @return array|mixed
      */
-    public static function get(string $path)
+    public function value(string $path)
     {
-        $module = self::resolveModule();
+        $module = $this->module ?? $this->resolveModule();
 
-        if ( empty( self::$modules[ $module ] ) ) {
-            self::$modules[ $module ] = Configuration::getInstance( $module );
+        if ( empty( $this->configurations[ $module ] ) ) {
+            $this->configurations[ $module ] = Configuration::getInstance( $module );
         }
 
-        return data_get( self::$modules[ $module ], $path );
+        return data_get( $this->configurations[ $module ], $path );
     }
 
     /**
      * @return string|null
      */
-    protected static function resolveModule(): ?string
+    protected function resolveModule(): ?string
     {
-        $caller = debug_backtrace( DEBUG_BACKTRACE_IGNORE_ARGS )[1]['file'];
+        $caller = debug_backtrace( DEBUG_BACKTRACE_IGNORE_ARGS )[2]['file'];
 
         if ( Str::contains( $caller, 'local/components' ) ) {
             $slice = Str::after( $caller, Application::getDocumentRoot() . '/local/components/' );
